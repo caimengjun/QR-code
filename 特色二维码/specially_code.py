804ec463273086e7b5b5f03396470218f337961a
CaiMengjun <1299851090>
2018-08-25 10:38:51 +0800

    QR_code

---
+++ 特色二维码/specially_code.py
@@ -0,0 +1,13 @@
+from MyQR import myqr
+
+
+a = myqr.run(words="https://github.com/caimengjun",
+             picture="a.gif",
+             colorized=True,
+             save_name="superman.gif"
+             )
+
+print(a)
+
+
+
